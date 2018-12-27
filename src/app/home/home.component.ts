665753b23767bf141db3a93c6f1dafe429558cd0
tania <strekasia@tut.by>
2018-12-27 13:56:34 +0300

    feat: add custom observable

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,25 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Observable} from "rxjs/Observable";
 import 'rxjs/Rx';
+import {Observer} from "rxjs";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
-  constructor() {
+    constructor() {
 
-  }
+    }
 
-  ngOnInit() {
-      const myNumbers = Observable.interval(1000);
-      myNumbers.subscribe(
-          (number: number) => {
-              console.log(number);
-          }
-      );
-  }
+    ngOnInit() {
+        const myObservable = Observable.create((observer: Observer<string>) => {
+            setTimeout(() => {
+                observer.next('first package');
+            }, 2000);
+            setTimeout(() => {
+                observer.next('second package');
+            }, 4000);
+            setTimeout(() => {
+                // observer.error('this does not work');
+                observer.complete();
+            }, 5000);
+            setTimeout(() => {
+                observer.next('third package');
+            }, 6000);
+        });
+        myObservable.subscribe(
+            (data: string) => { console.log(data); },
+            (error: string) => { console.log(error); },
+            () => { console.log('completed'); }
+        );
+    }
 
 }
